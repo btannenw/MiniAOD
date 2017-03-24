e8fc71168d4d1b6afd006c3d6a6e94f2f0d8708b
Satoshi Hasegawa <satoshi@fnal.gov>
2017-03-24 07:18:45 -0500

    electron id electron80XCutBasedV(Veto) is added.

--- MiniAODHelper/src/MiniAODHelper.cc
+++ MiniAODHelper/src/MiniAODHelper.cc
@@ -1158,6 +1158,7 @@ MiniAODHelper::isGoodElectron(const pat::Electron& iElectron, const float iMinPt
     passesIso=0.15>=GetElectronRelIso(iElectron, coneSize::R03, corrType::rhoEA,effAreaType::spring15);
     break;
 
+  case electronID::electron80XCutBasedV:
   case electronID::electron80XCutBasedL:
   case electronID::electron80XCutBasedM:
   case electronID::electron80XCutBasedT:
@@ -1745,6 +1746,30 @@ bool MiniAODHelper::PassElectron80XId(const pat::Electron& iElectron, const elec
 	       expectedMissingInnerHits <= 1 &&
 	       passConversionVeto &&
 	       relIso < 0.0571
+	       );
+    }
+    break;
+  case electronID::electron80XCutBasedV:
+    if( isEB ){
+      pass = ( full5x5_sigmaIetaIeta < 0.0115 &&
+	       fabsdEtaInSeed < 0.00749 &&
+	       dPhiIn < 0.228 &&
+	       hOverE < 0.356 &&
+	       ooEmooP < 0.299 &&
+	       expectedMissingInnerHits <= 2 &&
+	       passConversionVeto &&
+	       relIso < 0.175
+	       );
+    }
+    else{
+      pass = ( full5x5_sigmaIetaIeta < 0.037 &&
+	       fabsdEtaInSeed < 0.00895 &&
+	       dPhiIn < 0.213 &&
+	       hOverE < 0.211 &&
+	       ooEmooP < 0.15 &&
+	       expectedMissingInnerHits <= 3 &&
+	       passConversionVeto &&
+	       relIso < 0.159
 	       );
     }
     break;
